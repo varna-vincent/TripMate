2431bd23b5bb56b9433363700bddbfe04afede41
varna-vincent <vincent.varna@gmail.com>
2018-05-25 23:48:53 -0700

    Initial Project setup and Launching activity

---
+++ app/src/main/java/com/tripmate/coen268/tripmate/Login.java
@@ -0,0 +1,22 @@
+package com.tripmate.coen268.tripmate;
+
+import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.root_layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+    }
+}
